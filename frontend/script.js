a8a69b52eaea1fa4d7aac9804a931a3361445fad
mohammedwasifali <mdwali0912@gmail.com>
2026-03-15 21:11:03 +0530

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("patientForm").addEventListener("submit", function(e){
-
-e.preventDefault();
-
-alert("Patient data submitted successfully (backend not connected yet)");
-
-});
